778c7016e945963faacc9f3b183a1621982616b7
thanhldt6295 <thanhldt6295@gmail.com>
2017-01-13 16:30:36 +0700

    Update something

--- app/src/main/java/demo/example/thanhldtse61575/hotelservicetvbox/OrderActivity.java
+++ app/src/main/java/demo/example/thanhldtse61575/hotelservicetvbox/OrderActivity.java
@@ -1,9 +1,16 @@
 package demo.example.thanhldtse61575.hotelservicetvbox;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -15,5 +22,29 @@ public class OrderActivity extends AppCompatActivity {
         getSupportActionBar().setCustomView(R.layout.layout_actionbar);
         TextView abTitle=(TextView)findViewById(getResources().getIdentifier("action_bar_title", "id", getPackageName()));
         abTitle.setText("YOUR ORDER");
+
+        // Datetime & Calendar
+        TextView txtDate;
+        txtDate = (TextView) findViewById(R.id.txtDate);
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        txtDate.setText(currentDateTimeString);
+
+        final Calendar myCalen = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalen.set(Calendar.YEAR, year);
+                myCalen.set(Calendar.MONTH, monthOfYear);
+                myCalen.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            }
+        };
+
+        txtDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(OrderActivity.this,date,myCalen.get(Calendar.YEAR), myCalen.get(Calendar.MONTH),
+                        myCalen.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
     }
 }
