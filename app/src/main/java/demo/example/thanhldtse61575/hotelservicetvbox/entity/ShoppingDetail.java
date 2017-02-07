469c7b2fc4092f6d88e8ed215a5c148cba9dcfd8
thanhldt6295 <thanhldt6295@gmail.com>
2017-02-07 16:31:32 +0700

    Add ShoppingDetail

---
+++ app/src/main/java/demo/example/thanhldtse61575/hotelservicetvbox/entity/ShoppingDetail.java
@@ -0,0 +1,23 @@
+package demo.example.thanhldtse61575.hotelservicetvbox.entity;
+
+/**
+ * Created by ThanhLDTSE61575 on 2/7/2017.
+ */
+
+public class ShoppingDetail {
+    private String name;
+    private int image;
+
+    public ShoppingDetail(String name, int image){
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
